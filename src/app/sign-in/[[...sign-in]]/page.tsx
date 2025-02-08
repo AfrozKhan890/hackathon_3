bea0ecbf2c1c8d204cdee0a57718ede5e8d47bad
Afroz Khan <Afrozaftab11@gmail.com>
2025-02-08 12:56:49 +0500

    sanity data is remaining

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import {SignIn} from "@clerk/nextjs"
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+    <SignIn appearance={{ elements: { footer: "hidden" } }}/>
+    </div>
+  )
+}
+
+export default Home
